ebf0d5e834d78b79dbec6b3b0266ff952cb10cb0
Lucas Barroso <lucasbarroso23@gmail.com>
2020-09-10 00:21:01 -0300

    finalizando css pagina de cadastro de produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,10 @@
 module.exports = {
     create(req, res) {
+        //pegar categorias
         return res.render("products/create.njk")
+    },
+    post(req, res) {
+        //lógica de salvar
+
     }
 }
