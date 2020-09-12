8abe99d51c0371f3c08e728ee2d74ddb3f85a3ee
Lucas Barroso <lucasbarroso23@gmail.com>
2020-09-11 23:57:05 -0300

    products e catagory models/controllers

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,10 +1,39 @@
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+
 module.exports = {
     create(req, res) {
-        //pegar categorias
-        return res.render("products/create.njk")
+        //pegar categorias *Formato Promisse*
+        Category.all()
+            .then(function (results) {
+                const categories = results.rows;
+
+                return res.render("products/create.njk", { categories })
+
+            }).catch(function (err) {
+                throw new Error(err)
+            })
+
+
+
     },
-    post(req, res) {
-        //lógica de salvar
+    async post(req, res) {
+        //lógica de salvar *async await
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please. fill all fields!')
+            }
+        }
+        let results = await Product.create(req.body)
+        const product = results.rows[0]
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render("products/create.njk", { product, categories })
+
 
     }
 }
