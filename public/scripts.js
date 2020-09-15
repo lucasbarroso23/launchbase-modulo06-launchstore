fe16e4a3bf8f662a32a4fee73d841fd599243eb1
Lucas Barroso <lucasbarroso23@gmail.com>
2020-09-15 14:44:20 -0300

    gerenciamento de upload de imagens

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-//               MODO DE SE FAZER 1
+//               MODO DE SE FAZER 1 mask
 // const input = document.querySelector('input[name="price"]')
 // input.addEventListener("keydown", function (e) {
 
@@ -17,54 +17,114 @@
 //     }, 1);
 // })
 
-//              MODO DE SER FAZER 2
+//              MODO DE SER FAZER 2 mask
 
 const Mask = {
     apply(input, func) {
         setTimeout(() => {
             input.value = Mask[func](input.value)
         }, 1);
-    }, 
+    },
     formatBRL(value) {
         value = value.replace(/\D/g, "")
 
         return new Intl.NumberFormat('pt-BR', {
-            style: 'currency', 
+            style: 'currency',
             currency: 'BRL'
-        }).format(value/100)
+        }).format(value / 100)
     }
 }
 
 const PhotosUpload = {
+    input: "", 
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
+    files: [],
     handleFileInput(event) {
-        const { files: fileList } = event.target
-        const { uploadLimit } = PhotosUpload
+        const { files: fileList } = event.target     
+        PhotosUpload.input = event.target   
 
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault()
-            return
-        }
+        if(PhotosUpload.hasLimit(event)) return
+       
 
         Array.from(fileList).forEach(file => {
+
+            PhotosUpload.files.push(file)
+
             const reader = new FileReader()
 
             reader.onload = () => {
                 const image = new Image()
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
+                const div = PhotosUpload.getContainer(image)
 
-                div.onclick = () => alert('remover foto')
-
-                div.appendChild(image)
-
-                document.querySelector('#photos-preview').appendChild(div)
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
         })
+    
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick =  PhotosUpload.removePhoto
+
+        div.appendChild(image)
+
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+    hasLimit(event) {
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
+ 
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if(item.classList && item.classList.value == "photo")
+                photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if(totalPhotos > uploadLimit) {
+            alert("Você atingiu o limite máximo de fotos")
+            event.preventDefault()
+            return true 
+        }
+
+        return false
+    },
+    getAllFiles() {
+        const dataTranfer = new ClipboardEvent("").clipboardData || new dataTranfer
+
+        PhotosUpload.files.forEach(file => dataTranfer.items.add(file))
+
+        return dataTranfer.files
+    },
+    getRemoveButton () {
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = 'close'
+        return button
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index  = photosArray.indexOf(photoDiv)
+
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoDiv.remove()
     }
 }
